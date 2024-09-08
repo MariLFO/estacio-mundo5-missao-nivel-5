3c0745d90c650aecd4c9077160300e2ebeece867
MariLFO <mari.fer.onorio@gmail.com>
2024-09-07 18:38:07 -0700

    Extraído códigos do banco de dados para um arquivo separado

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
-const sqlite3 = require('sqlite3').verbose();
+const db = require('./dbSetup'); // Importa a configuração do banco de dados
 
 const app = express();
 
@@ -91,34 +91,6 @@ app.get('/api/contracts/:empresa/:inicio', authenticateToken, authorizeAdmin, as
   } catch (error) {
     res.status(500).json({ message: 'Internal Server Error' });
   }
-});
-
-// Configuração do banco de dados
-const db = new sqlite3.Database(':memory:');
-
-// Criação das tabelas de contratos e usuários
-db.serialize(() => {
-  db.run(`CREATE TABLE contracts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    empresa TEXT,
-    data_inicio TEXT
-  )`);
-
-  db.run(`CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT,
-    password TEXT,
-    email TEXT,
-    perfil TEXT
-  )`);
-
-  // Inserção de dados de exemplo
-  db.run(`INSERT INTO contracts (empresa, data_inicio) VALUES ('empresa1', '2023-01-01')`);
-  db.run(`INSERT INTO contracts (empresa, data_inicio) VALUES ('empresa2', '2023-02-01')`);
-
-  db.run(`INSERT INTO users (username, password, email, perfil) VALUES ('user', '123456', 'user@dominio.com', 'user')`);
-  db.run(`INSERT INTO users (username, password, email, perfil) VALUES ('admin', '123456789', 'admin@dominio.com', 'admin')`);
-  db.run(`INSERT INTO users (username, password, email, perfil) VALUES ('colab', '123', 'colab@dominio.com', 'user')`);
 });
 
 // Função genérica para executar consultas SQL
